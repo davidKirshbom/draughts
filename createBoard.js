627f6ab76ba7b1e09389ba4406e248910165f36f
david kirshbom <davidkirshbomloy@gmail.com>
2020-08-04 20:25:39 +0300

    board complete

---
+++ createBoard.js
@@ -0,0 +1,37 @@
+let board = document.getElementsByClassName("board")[0]
+for(let row=1;row<=8;row++)
+{
+ for(let column=1;column<=8;column++)
+ {
+     let square = document.createElement("div");
+     let piece = document.createElement("span");
+     if ((row + column) % 2 != 0) {
+         if (row <= 3)
+             piece.color = "red"
+         if (row >= 6)
+             piece.color = "grey"
+     }
+     if(row%2==0){
+     if(column%2==0)
+      square.color="white" 
+       else
+       square.color="black"
+     }
+     else
+     {
+       if(column%2==0)
+      square.color="black" 
+       else
+       square.color="white"
+     }
+     square.setAttribute("class", "square " + square.color);
+     if(piece.color)
+     piece.setAttribute("class","piece "+piece.color)
+     piece.style.gridRow = `${row}/${row + 1}`
+     piece.style.gridColumn=`${column}/${column}`
+     square.style.gridRow=`${row}/${row+1}`
+     square.style.gridColumn=`${column}/${column}`
+     board.appendChild(square);
+     board.appendChild(piece)
+ }
+}
